eef85076f393540b6b7bfea2ea0ab04226e0e624
AnshPatel-SoftDev2704 <anshkumar.patel@roimaint.com>
2025-02-12 11:26:56 +0530

    Created Single Controller for the Interview Funcationality

--- Recruitment_Process_Management_System/Controller/InterviewController.cs
+++ Recruitment_Process_Management_System/Controller/InterviewController.cs
@@ -10,15 +10,9 @@ namespace Recruitment_Process_Management_System.Controllers
     public class InterviewController : ControllerBase
     {
         private readonly IInterviewService _interviewService;
-        private readonly ICandidate_Application_StatusService _candidate_Application_StatusService;
-        private readonly IInterview_TypeService _interview_TypeService;
-        private readonly IInterview_StatusService _interview_StatusService;
-        public InterviewController(IInterviewService interviewService,ICandidate_Application_StatusService candidate_Application_StatusService,IInterview_StatusService interview_StatusService,IInterview_TypeService interview_TypeService)
+        public InterviewController(IInterviewService interviewService)
         {
             _interviewService = interviewService;
-            _candidate_Application_StatusService = candidate_Application_StatusService;
-            _interview_StatusService = interview_StatusService;
-            _interview_TypeService = interview_TypeService;
         }
 
         [HttpGet]
@@ -100,7 +94,265 @@ namespace Recruitment_Process_Management_System.Controllers
             {
                 return BadRequest(ex.Message);
             }
+        }
+
+        [HttpGet("GetAllInterviewStatus")]
+        public async Task<ActionResult<IEnumerable<Interview_Status>>> getAllInterview_Status()
+        {
+            try{
+                var response = await _interviewService.getAllInterview_Status();
+                return Ok(response);
+            }
+            catch(Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpGet("GetInterviewStatusById/{Interview_Status_id}")]
+        public async Task<ActionResult<Interview_Status>> getInterview_StatusById(int Interview_Status_id)
+        {
+            try{
+                var response = await _interviewService.getInterview_StatusById(Interview_Status_id);
+                if(response == null)
+                return NotFound("Interview Status Not Found");
+                else
+                return Ok(response);
+            }
+            catch(Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
             
+        }
+
+        [HttpPost("SaveInterviewStatus")]
+        public async Task<ActionResult<Interview_Status>> saveInterview_Status(Interview_Status interview_Status)
+        {
+            try{
+                var response = await _interviewService.saveInterview_Status(interview_Status);
+                if(response == null)
+                return NotFound("Something Went Wrong.");
+                else
+                return Ok(response);
+            }
+            catch(Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpGet("GetAllInterviewType")]
+        public async Task<ActionResult<IEnumerable<Interview_Type>>> getAllInterview_Type()
+        {
+            try{
+                var response = await _interviewService.getAllInterview_Type();
+                return Ok(response);
+            }
+            catch(Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpGet("GetInterviewTypeById/{Interview_Type_id}")]
+        public async Task<ActionResult<Interview_Type>> getInterview_TypeById(int Interview_Type_id)
+        {
+            try{
+                var response = await _interviewService.getInterview_TypeById(Interview_Type_id);
+                if(response == null)
+                return NotFound("Interview Type Not Found");
+                else
+                return Ok(response);
+            }
+            catch(Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+            
+        }
+
+        [HttpPost("SaveInterviewType")]
+        public async Task<ActionResult<Interview_Type>> saveInterview_Type(Interview_Type interview_Type)
+        {
+            if(!ModelState.IsValid)
+            return BadRequest(ModelState);
+            try{
+                var response = await _interviewService.saveInterview_Type(interview_Type);
+                if(response == null)
+                return BadRequest("Something Went Wrong");
+                else
+                return Ok(response);
+            }
+            catch(Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+            
+        }
+
+        [HttpGet("GetAllInterviewer")]
+        public async Task<ActionResult<IEnumerable<Interviewer>>> getAllInterviewer()
+        {
+            try{
+                var responses = await _interviewService.getAllInterviewer();
+                return Ok(responses);
+            }
+            catch(Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpGet("GetInterviewerById/{Interviewer_id}")]
+        public async Task<ActionResult<Interviewer>> getInterviewerById(int Interviewer_id)
+        {
+            try{
+                var response = await _interviewService.getInterviewerById(Interviewer_id);
+                if(response == null)
+                return NotFound("Interviewer Not Found");
+                else
+                return Ok(response);
+            }
+            catch(Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+            
+        }
+        
+        [HttpPost("SaveInterviewer")]
+        public async Task<ActionResult<Interviewer>> saveInterviewer(InterviewerDTO interviewerDTO)
+        {
+            if(!this.ModelState.IsValid)
+            return BadRequest(ModelState);
+             try{
+                var response = await _interviewService.saveInterviewer(interviewerDTO);
+                return Ok(response);
+            }
+            catch(Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+            
+        }
+
+        [HttpPut("UpdateInterviewer{Interviewer_id}")]
+        public async Task<ActionResult<Interviewer>> updateInterviewer(int Interviewer_id,InterviewerDTO interviewerDTO)
+        {
+            if(!this.ModelState.IsValid)
+            return BadRequest(ModelState);
+            try{
+                var interviewer = await _interviewService.updateInterviewer(Interviewer_id,interviewerDTO);
+                if(interviewer == null)
+                return NotFound("Interviewer Not Found");
+                else
+                return Ok(interviewer);
+            }
+            catch(Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpDelete("DeleteInterviewer/{Interviewer_id}")]
+        public async Task<ActionResult<bool>> deleteInterviewer(int Interviewer_id)
+        {
+            try{
+                var response = await _interviewService.deleteInterviewer(Interviewer_id);
+                if(response)
+                return Ok("Interviewer Deleted Successfully");
+                else
+                return NotFound("Interviewer Not Found");
+            }
+            catch(Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpGet("GetAllFeedback")]
+        public async Task<ActionResult<IEnumerable<Feedback>>> getAllFeedback()
+        {
+            try
+            {
+                var responses = await _interviewService.getAllFeedback();
+                return Ok(responses);
+            }
+            catch(Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpGet("GetFeedbackById/{Feedback_id}")]
+        public async Task<ActionResult<Feedback>> getFeedbackById(int Feedback_id)
+        {
+            try{
+                var response = await _interviewService.getFeedbackById(Feedback_id);
+                if(response == null)
+                return NotFound("Feedback Not Found");
+                else
+                return Ok(response);
+            }
+            catch(Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPost("SaveFeedback")]
+        public async Task<ActionResult<Feedback>> saveFeedback(FeedbackDTO feedbackDTO)
+        {
+            if(!this.ModelState.IsValid)
+            return BadRequest(ModelState);
+
+            try{
+                var response = await _interviewService.saveFeedback(feedbackDTO);
+                if(response == null)
+                return NotFound("Something Went wrong");
+                else
+                return Ok(response);
+            }
+            catch(Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPut("UpdateFeedback/{Feedback_id}")]
+        public async Task<ActionResult<Feedback>> updateFeedback(int Feedback_id,FeedbackDTO feedbackDTO)
+        {
+            if(!this.ModelState.IsValid)
+            return BadRequest(ModelState);
+
+            try{
+                var response = await _interviewService.updateFeedback(Feedback_id,feedbackDTO);
+                if(response == null)
+                return NotFound("Something Went wrong");
+                else
+                return Ok(response);
+            }
+            catch(Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpDelete("DeleteFeedback/{Feedback_id}")]
+        public async Task<ActionResult<Feedback>> deleteFeedback(int Feedback_id)
+        {
+            try{
+                var response = await _interviewService.deleteFeedback(Feedback_id);
+                if(!response)
+                return NotFound("Something Went wrong");
+                else
+                return Ok("Feedback Deleted Successfully");
+            }
+            catch(Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
         }
     }
 }
--- Recruitment_Process_Management_System/Services/InterivewService.cs
+++ Recruitment_Process_Management_System/Services/InterivewService.cs
@@ -12,21 +12,29 @@ namespace Recruitment_Process_Management_System.Services
         private readonly IInterview_TypeRepository _interview_TypeRepository;
         private readonly IInterview_StatusRepository _interview_StatusRepository;
         private readonly INotification_CandidateService _notification_CandidateService;
-        private readonly IInterviewerService _interviewerService;
+        private readonly IInterviewerRepository _interviewerRepository;
+        private readonly IUserRepository _userRepository;
+        private readonly INotifications_UsersService _notifications_UsersService;
+                private readonly IFeedbackRepository _feedbackRepository;
+
 
         public InterviewService(IInterviewRepository interviewRepository,
         ICandidate_Application_StatusRepository candidate_Application_StatusRepository,
         IInterview_StatusRepository interview_StatusRepository,
         IInterview_TypeRepository interview_TypeRepository,
         INotification_CandidateService notification_CandidateService,
-        IInterviewerService interviewerService)
+        IInterviewerRepository interviewerRepository,IFeedbackRepository feedbackRepository,
+        IUserRepository userRepository,INotifications_UsersService notifications_UsersService)
         {
             _interviewRepository = interviewRepository;
             _candidate_Application_StatusRepository = candidate_Application_StatusRepository;
             _interview_StatusRepository = interview_StatusRepository;
             _interview_TypeRepository = interview_TypeRepository;
             _notification_CandidateService = notification_CandidateService;
-            _interviewerService = interviewerService;
+            _interviewerRepository = interviewerRepository;
+            _userRepository = userRepository;
+            _notifications_UsersService = notifications_UsersService;
+            _feedbackRepository = feedbackRepository;
         }
 
         public async Task<bool> deleteInterview(int Interview_id) {
@@ -103,7 +111,7 @@ namespace Recruitment_Process_Management_System.Services
             if(!(existingInterview.Scheduled_at != interviewDTO.Scheduled_at))
             {
                 await sendNotification(existingInterview,"Rescheduled");
-                await _interviewerService.interviewUpdated(existingInterview,"Rescheduled");
+                await interviewUpdated(existingInterview,"Rescheduled");
             }
             return await _interviewRepository.updateInterview(existingInterview);
         }
@@ -121,6 +129,163 @@ namespace Recruitment_Process_Management_System.Services
             await _notification_CandidateService.saveCandidateNotification(notifications_CandidateDTO);
         }
 
-        
+        public async Task<IEnumerable<Interview_Status>> getAllInterview_Status() => await _interview_StatusRepository.getAllInterview_Status();
+
+        public async Task<Interview_Status> getInterview_StatusById(int Interview_Status_id) => await _interview_StatusRepository.getInterview_StatusById(Interview_Status_id);
+
+        public async Task<Interview_Status> saveInterview_Status(Interview_Status interview_Status) {
+
+            return await _interview_StatusRepository.saveInterview_Status(interview_Status);
+        }
+
+        public async Task<IEnumerable<Interview_Type>> getAllInterview_Type() => await _interview_TypeRepository.getAllInterview_Type();
+
+        public async Task<Interview_Type> getInterview_TypeById(int Interview_Type_id) => await _interview_TypeRepository.getInterview_TypeById(Interview_Type_id);
+
+        public async Task<Interview_Type> saveInterview_Type(Interview_Type interview_Type) {
+           return await _interview_TypeRepository.saveInterview_Type(interview_Type);
+        }
+
+         public async Task<bool> deleteInterviewer(int Interviewer_id) {
+            var response = await _interviewerRepository.getInterviewerById(Interviewer_id);
+            if(response == null)
+            return false;
+            return await _interviewerRepository.deleteInterviewer(response);
+        }
+
+        public async Task<IEnumerable<Interviewer>> getAllInterviewer() => await _interviewerRepository.getAllInterviewer();
+
+        public async Task<Interviewer> getInterviewerById(int Interviewer_id) => await _interviewerRepository.getInterviewerById(Interviewer_id);
+
+        public async Task<Interviewer> saveInterviewer(InterviewerDTO interviewerDTO) {
+            var newInterview = await _interviewRepository.getInterviewById(interviewerDTO.Interview_id);
+            var newUser = await _userRepository.getUserById(interviewerDTO.User_id);
+
+            if(newInterview == null)
+            throw new Exception("Interview Not Found");
+
+            if(newUser == null)
+            throw new Exception("User Not Found");
+
+            Interviewer interviewer = new Interviewer{
+                Interview_id = interviewerDTO.Interview_id,
+                interview = newInterview,
+                User_id = interviewerDTO.User_id,
+                user = newUser,
+                Created_at = DateTime.Now,
+                Updated_at = DateTime.Now
+            };
+
+            await sendNotification(interviewer,"Scheduled");
+            return await _interviewerRepository.saveInterviewer(interviewer);
+        }
+
+        public async Task<Interviewer> updateInterviewer(int Interviewer_id, InterviewerDTO interviewerDTO) {
+            var newInterview = await _interviewRepository.getInterviewById(interviewerDTO.Interview_id);
+            var newUser = await _userRepository.getUserById(interviewerDTO.User_id);
+            var existingInterviewer = await _interviewerRepository.getInterviewerById(Interviewer_id);
+
+            if(newInterview == null)
+            throw new Exception("Interview Not Found");
+
+            if(newUser == null)
+            throw new Exception("User Not Found");
+
+            if(existingInterviewer == null)
+            throw new Exception("Interviewer Not Found");
+
+            existingInterviewer.Interview_id = interviewerDTO.Interview_id;
+            existingInterviewer.interview = newInterview;
+            existingInterviewer.User_id = interviewerDTO.User_id;
+            existingInterviewer.user = newUser;
+            existingInterviewer.Updated_at = DateTime.Now;
+            return await _interviewerRepository.updateInterviewer(existingInterviewer);
+        }
+
+        public async Task interviewUpdated(Interview interview,string Action)
+        {
+            var responses = await _interviewerRepository.getAllInterviewer();
+            var existingInterview = responses.FirstOrDefault(r => r.Interview_id == interview.Interview_id);
+            existingInterview.interview = interview;
+            await sendNotification(existingInterview,Action);
+        }
+
+        public async Task sendNotification(Interviewer interviewer,string Action)
+        {
+            string message = "You Need to take the interview for Job Title " + interviewer.interview.candidate_Application_Status.job.Job_title + " which is " + Action + " on " + interviewer.interview.Scheduled_at.ToString() + " \n The Interview Type is " + interviewer.interview.interview_Type.Interview_Type_name + " and The Round Number is " + interviewer.interview.Number_of_round.ToString();
+            Notifications_UsersDTO notifications_UsersDTO = new Notifications_UsersDTO{
+                User_id = interviewer.User_id,
+                Message = message,
+                Status = false
+            };
+
+            await _notifications_UsersService.saveUserNotification(notifications_UsersDTO);
+        }
+
+        public async Task<bool> deleteFeedback(int Feedback_id)
+        {
+            var feedback = await _feedbackRepository.getFeedbackById(Feedback_id);
+            if(feedback == null)
+            return false;
+
+            return await _feedbackRepository.deleteFeedback(feedback);
+        }
+
+        public async Task<IEnumerable<Feedback>> getAllFeedback() => await _feedbackRepository.getAllFeedback();
+
+        public async Task<Feedback> getFeedbackById(int Feedback_id) => await _feedbackRepository.getFeedbackById(Feedback_id);
+
+        public async Task<Feedback> saveFeedback(FeedbackDTO feedbackDTO)
+        {
+            var newInterview = await _interviewRepository.getInterviewById(feedbackDTO.Interview_id);
+            if(newInterview == null)
+            throw new Exception("Interview Not Found");
+
+            var newUser = await _userRepository.getUserById(feedbackDTO.User_id);
+            if(newUser == null)
+            throw new Exception("User Not Found");
+
+            Feedback feedback = new Feedback{
+                Interview_id = feedbackDTO.Interview_id,
+                interview = newInterview,
+                User_id = feedbackDTO.User_id,
+                user = newUser,
+                Technology = feedbackDTO.Technology,
+                rating = feedbackDTO.rating,
+                comments = feedbackDTO.comments,
+                submitted_date = feedbackDTO.submitted_date,
+                Created_at = DateTime.Now,
+                Updated_at = DateTime.Now
+            };
+
+            return await _feedbackRepository.saveFeedback(feedback);
+        }
+
+        public async Task<Feedback> updateFeedback(int Feedback_id, FeedbackDTO feedbackDTO)
+        {
+            var newInterview = await _interviewRepository.getInterviewById(feedbackDTO.Interview_id);
+            if(newInterview == null)
+            throw new Exception("Interview Not Found");
+
+            var newUser = await _userRepository.getUserById(feedbackDTO.User_id);
+            if(newUser == null)
+            throw new Exception("User Not Found");
+
+            var existingFeedback = await _feedbackRepository.getFeedbackById(Feedback_id);
+            if(existingFeedback == null)
+            throw new Exception("Feedback Not Found");
+
+            existingFeedback.Interview_id = feedbackDTO.Interview_id;
+            existingFeedback.interview = newInterview;
+            existingFeedback.User_id = feedbackDTO.User_id;
+            existingFeedback.user = newUser;
+            existingFeedback.Technology = feedbackDTO.Technology;
+            existingFeedback.rating = feedbackDTO.rating;
+            existingFeedback.comments = feedbackDTO.comments;
+            existingFeedback.submitted_date = feedbackDTO.submitted_date;
+            existingFeedback.Updated_at = DateTime.Now;
+
+            return await _feedbackRepository.updateFeedback(existingFeedback);
+        }
     }
 }
